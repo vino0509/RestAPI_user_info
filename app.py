f514e3618bb133aa7204d2859ac4fd69b928213f
vino0509 <164057352+vino0509@users.noreply.github.com>
2025-08-23 22:23:04 +0530

    Update app.py
    added Authentication

--- app.py
+++ app.py
@@ -4,7 +4,27 @@ import os
 app = Flask(__name__)
 
 # Fake DB (will reset on restart)
-users = [{"id": 1, "name": "https://station-h.hexaware.com/apps/featured"}, {"id": 2, "name": "https://station-h.hexaware.com/apps/categories/1420"}]
+#users = [{"id": 1, "name": "https://station-h.hexaware.com/apps/featured"}, {"id": 2, "name": "https://station-h.hexaware.com/apps/categories/1420"}]
+users = [{"id": 1, "name": "Vinoth"}, {"id": 2, "name": "Sara"}]
+
+
+# Define your API key (you can make it an environment variable for security)
+API_KEY = "vinothbaburajkumar"
+
+# Middleware to check API key
+@app.before_request
+def check_api_key():
+    # Skip checking for the home route
+    if request.endpoint == 'home':
+        return
+    # Get API key from headers
+    key = request.headers.get("api-key")
+    if key != API_KEY:
+        abort(401, description="Unauthorized: Invalid or missing API key")
+
+
+
+
 
 @app.route('/')
 def home():
@@ -30,5 +50,6 @@ if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))  # Use Render's PORT
     app.run(host='0.0.0.0', port=port)
     app.run(debug=True)
+
 
 
